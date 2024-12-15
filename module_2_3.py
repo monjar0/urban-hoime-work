b8c846b11772abf1f47cda13047165115cc0541b
monjar0 <raimbaev.viktor@gmail.com>
2024-12-15 22:24:05 +0500

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -3,7 +3,10 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 result = []
 i = 0
 
-while i < len(my_list) and my_list[i] >0:
-    result.append(my_list[i])
+while i < len(my_list):
+    if my_list[i] < 0:
+        break
+    if my_list[i] > 0:
+        result.append(my_list[i])
     i += 1
 print(result)
